5a0fd92d0fbc0531b0cec6f98208142b18ce77d9
yday05506 <yangdayeon2@gmail.com>
2023-04-17 11:57:27 +0900

    Console.SetCursorPosition 연습

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,13 +10,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕 세상아");
-            char a = 'a';
-            Console.WriteLine(a);   // 와 자동완성...
-            string b = "b";
-            Console.WriteLine(b);   // 와... 자동완성...
+            //Console.WriteLine("안녕 세상아");
+            //char a = 'a';
+            //Console.WriteLine(a);   // 와 자동완성...
+            //string b = "b";
+            //Console.WriteLine(b);   // 와... 자동완성...
 
-            foreach(var arg in args) Console.WriteLine(arg);
+            //foreach(var arg in args) Console.WriteLine(arg);
+
+
+            // API 연습
+            Console.Write("메서드 호출");
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출2");
+            Console.SetCursorPosition(2, 2);
+            Console.Write("메서드 호출3");
+            // Console.SetCursorPosition(5, 5);
         }
     }
 }
