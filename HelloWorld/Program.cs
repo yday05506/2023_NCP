fdb0e855d847db9f22482a20514ed95023a35b08
yday05506 <yangdayeon2@gmail.com>
2023-04-17 12:18:09 +0900

    여러가지 콘솔 메서드를 사용해 스네이크 움직임 구현

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,14 +22,30 @@ namespace HelloWorld
 
 
             // API 연습
-            Console.Write("메서드 호출");
-            Thread.Sleep(1000);
-            Console.SetCursorPosition(5, 5);
-            Console.Write("메서드 호출2");
-            Thread.Sleep(2000);
-            Console.SetCursorPosition(2, 2);
-            Console.Write("메서드 호출3");
+            //Console.Write("메서드 호출");
+            //Thread.Sleep(1000);
+            //Console.SetCursorPosition(5, 5);
+            //Console.Write("메서드 호출2");
+            //Thread.Sleep(2000);
+            //Console.SetCursorPosition(2, 2);
+            //Console.Write("메서드 호출3");
             // Console.SetCursorPosition(5, 5);
+
+            int x = 1;
+            while(x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if(x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if(x%3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+                Thread.Sleep(1000);
+                x++;
+            }
         }
     }
 }
